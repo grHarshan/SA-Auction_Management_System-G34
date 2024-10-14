77f7b377299a0257ae84a2c20510881a4dfedfca
Ruwanthi Yashodara <yashodararuwanthi@gmail.com>
2024-10-14 12:11:14 +0530

    update ContactUs jsx file

---
+++ src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ContactUs.css';
+
+const ContactUs = () => {
+    return ( 
+        <div className="contact">
+            <h1>Contact US</h1>
+        </div>
+     );
+}
+ 
+export default ContactUs;
