7f19aa8384da7509f981031f9a7fadf6d550a939
Ruwanthi Yashodara <yashodararuwanthi@gmail.com>
2024-10-17 17:47:36 +0530

    form texture

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -15,11 +15,30 @@ const ContactUs = () => {
         </div>
       </div>
 
+      {/*title section*/}
+
       <div className='title'>
-        <div className='tagline'></div>
+        <div className='tagline'>Tagline</div>
         <div className='ContactUs'>Contact Us</div>
       </div>
+
+      {/*input section*/}
+      <div className='Inputs'>
+        <div className='input'>
+        <div className='name'>Name</div>
+        <input type='text'></input>
+        <div className='email'>Email</div>
+        <input type='email'></input>
+        <div className='message'>Message</div>
+        <input type='text'></input>
+
+        </div>
+        
+
       </div>
+
+      </div>
+      
 
      );
 }
