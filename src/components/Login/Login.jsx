591b548a822b3872508f30c776c4b3b9e2e56c6c
SNimsara <nimsaragunasekara123@gmail.com>
2024-10-21 11:35:52 +0530

    edit login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
               <input type="text" id="name" placeholder="Enter your name" required />
             </div>
             <div className="form-group">
-              <label htmlFor="password">Email*</label>
+              <label htmlFor="password">Password*</label>
               <input type="password" id="password" placeholder="Enter your password" required />
             </div>
             
