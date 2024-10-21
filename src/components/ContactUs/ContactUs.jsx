2c9c656827639346fc0087919b62e519750357b9
Ruwanthi Yashodara <yashodararuwanthi@gmail.com>
2024-10-21 16:34:58 +0530

    add acknowledgment for contact form submission.

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState,useRef } from 'react';
 import './ContactUs.css';
-import { Link } from 'react-router-dom';
+
 import bg2 from '../../assets/bg2.png';
 import phone from '../../assets/phone.jpg';
 import mail from '../../assets/mail.jpg';
@@ -8,7 +8,30 @@ import locate from '../../assets/locate.jpg';
 
 
 const ContactUs = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [submitted, setSubmitted] = useState(false); 
+
+  
+  const formRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    setSubmitted(true); 
+
+    
+    setName('');
+    setEmail('');
+    setMessage('');
+
+    
+    formRef.current.reset();
+    //setTimeout(() => setSubmitted(false), 3000);
+    
+  };
     return ( 
+      
         <div >
       {/* Background Image Section */}
       <div className="bg2">
@@ -25,19 +48,21 @@ const ContactUs = () => {
             </div>
 
             {/* Input Section */}
-            <div className='inputs-container'>
+            {submitted && <div className='message'>Your response was recorded.</div>} 
+
+            <form className='inputs-container' onSubmit={handleSubmit} ref={formRef}>
                 <div className='input'>
                     <label className='label' htmlFor='name'>Name</label>
-                    <input type='text' id='name' placeholder='Enter your name...' className='input-field' required />
+                    <input type='text' id='name' placeholder='Enter your name...' className='input-field' onChange={(e) => setName(e.target.value) }required />
 
                     <label className='label' htmlFor='email'>Email</label>
-                    <input type='email' id='email' placeholder='Enter your email...' className='input-field'  required/>
+                    <input type='email' id='email' placeholder='Enter your email...' className='input-field' onChange={(e) => setEmail(e.target.value)}  required/>
 
                     <label className='label' htmlFor='message'>Message</label>
-                    <textarea id='message' placeholder='Write your message...' className='textarea-field' required></textarea>
+                    <textarea id='message' placeholder='Write your message...' className='textarea-field' onChange={(e) => setMessage(e.target.value)} required></textarea>
 
                     <div className='checkbox-container'>
-                    <input type='checkbox' id='terms' required />
+                    <input type='checkbox' id='terms'  required />
                     <label htmlFor='terms'>I accept the Terms.</label>
                         
                     </div>
@@ -46,7 +71,7 @@ const ContactUs = () => {
                         <button type='submit' className='submit-btn'>Submit</button>
                     </div>
                 </div>
-                </div>
+                </form>
                 <div className='contact-icons'>
                   <div className='contact-item'>
                   <img src={phone} className='imgC'></img>
